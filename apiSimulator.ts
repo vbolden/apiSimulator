51319eafe11acae77e9503ddafaa7591fc8b69b9
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-05 09:56:44 -0400

    changed sample code to function instead of variable declaration

--- apiSimulator.ts
+++ apiSimulator.ts
@@ -1,17 +1,17 @@
-export const fetchProductCatalog = (): Promise<{ id: number; name: string; price: number }[]> => {
+function fetchProductCatalog(id: number, name: string, price: number) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (Math.random() < 0.8) {
                 resolve([
-                    { id: 1, name: "Laptop", price: 1200 },
-                    { id: 2, name: "Headphones", price: 200 },
+                    {id: 1, name: "Laptop", price: 1200},
+                    {id: 2, name: "Headphone", price: 150},
                 ]);
             } else {
-                reject("Failed to fetch product catalog");
+                reject("Failed to fetch product catalog")
             }
-        }, 1000);
-    });
-};
+        }, 1000)
+    })
+}
 
 function fetchProductReviews(productId: number): Promise<void> {
     return new Promise((resolve, reject) => {
